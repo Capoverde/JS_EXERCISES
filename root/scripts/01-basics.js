647fcb4d11d7f2fde281fb9ebba535f71947cf4f
Piotr <piotr_as@yahoo.com>
2018-07-24 15:57:44 +0200

    solved #41

--- root/scripts/01-basics.js
+++ root/scripts/01-basics.js
@@ -830,3 +830,21 @@
 //   console.log(check8(24, 32));
 //   console.log(check8(17, 18));
 
+// ---------------- Exe #41 ---------------- //
+
+// Write a JavaScript program to check three given numbers, if the three numbers are same return 30 otherwise return 40 and if two numbers are same return 20
+
+// function three_numbers(x, y, z) {
+//     if (x == y && y == z) {
+//       return 30;
+//     }
+  
+//     if (x == y || y == z || z == x) {
+//       return 40;
+//     }
+  
+//     return 20;
+//   }
+//   console.log(three_numbers(8, 8, 8));
+//   console.log(three_numbers(8, 8, 18));
+//   console.log(three_numbers(8, 7, 18));
