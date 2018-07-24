0fe442c4da4cbfc53dff93b553c64fd89070013a
Piotr <piotr_as@yahoo.com>
2018-07-24 16:01:11 +0200

    solved #41

--- root/scripts/01-basics.js
+++ root/scripts/01-basics.js
@@ -848,3 +848,27 @@
 //   console.log(three_numbers(8, 8, 8));
 //   console.log(three_numbers(8, 8, 18));
 //   console.log(three_numbers(8, 7, 18));
+
+// ---------------- Exe #42 ---------------- //
+
+// Write a JavaScript program to check if three given numbers (integers) are increasing in strict mode and flag is "false", however if "true" is false the numbers will in soft mode. 
+// Note: Strict mode -> 10, 15, 31 : Soft mode -> 24, 22, 31 or 22, 22, 31 
+
+// function number_order(x, y, z ) {
+//     if ( y > x && z > y) 
+//     {
+//       return "strict mode";    
+//     }
+//     else if(z > y) 
+//      return "Soft mode";
+//     else
+//       return "Undefinded";
+//   }
+  
+//   console.log(number_order(10,15,31));
+//   console.log(number_order(24,22,31));
+//   console.log(number_order(50,21,15));
+
+// ---------------- Exe #43 ---------------- //
+
+
