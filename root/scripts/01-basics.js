e3abe3cdc8b4d996f0f5266482546cce8bf5ec16
Piotr <piotr_as@yahoo.com>
2018-07-24 15:22:55 +0200

    #39 solved

--- root/scripts/01-basics.js
+++ root/scripts/01-basics.js
@@ -756,3 +756,34 @@
 
 // ---------------- Exe #39 ---------------- //
 
+// Write a JavaScript program to compute the sum of the two given integers, If the sum is in the range 50..80 return 65 other wise return 80.
+
+// My solution:
+
+// function sumRange(n1,n2)
+// {
+//     sum = n1 + n2;
+//     if (sum >= 50 && sum <= 80)
+//     {
+//         return 65;
+//     }
+//     else
+//     {
+//         return 80;
+//     }
+// }
+// sumRange(52,15);
+
+// Their solution:
+
+// function sortaSum(x, y) 
+//  {
+//   const sum_nums = x + y;
+//   if (sum_nums >= 50 && sum_nums <= 80) {
+//     return 65;
+//   }
+//   return 80;
+// }
+
+// console.log(sortaSum(30,20));
+// console.log(sortaSum(90,80));
