d79da28b7c5b73acfc736c577a7480d03aad235a
PiotrAs <piotr.slobinski@gmail.com>
2018-07-25 19:31:23 +0200

    started #62

--- root/scripts/01-basics.js
+++ root/scripts/01-basics.js
@@ -1271,3 +1271,8 @@
 //   console.log(summ);
 // }
 // con('Axxx', 'Byyy');
+
+// ---------------- Exe #62 ---------------- //
+
+// Write a JavaScript program to move last three character to the start of a given string. The string length must be greater or equal to three
+
