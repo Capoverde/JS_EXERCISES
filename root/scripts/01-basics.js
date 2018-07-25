1f9de4b91523d0b7fd155310ff88c7b02428f790
Piotr <piotr_as@yahoo.com>
2018-07-25 10:03:48 +0200

    solved #49

--- root/scripts/01-basics.js
+++ root/scripts/01-basics.js
@@ -999,13 +999,44 @@
 
 // Write a JavaScript program to replace every character in a given string with the character following it in the alphabet
 
-function stringPlus (str)
+// function string_reverse(str) 
+// function LetterChanges(text) {
+// //https://goo.gl/R8gn7u
+// var s = text.split('');
+// for (var i = 0; i < s.length; i++) {
+// // Caesar cipher
+// switch(s[i]) {
+// case ' ':
+// break;
+// case 'z':
+// s[i] = 'a';
+// break;
+// case 'Z': 
+// s[i] = 'A';
+// break;
+// default:
+// s[i] = String.fromCharCode(1 + s[i].charCodeAt(0));
+// }
+
+// // Upper-case vowels
+// switch(s[i]) {
+// case 'a': case 'e': case 'i': case 'o': case 'u':
+// s[i] = s[i].toUpperCase();
+// }
+// }
+// return s.join('');
+// }
+// console.log(LetterChanges("PYTHON"));
+// console.log(LetterChanges("W3R"));
+// console.log(LetterChanges("php"));
+
+
+// ---------------- Exe #50 ---------------- //
+
+// Write a JavaScript program to capitalize the first letter of each word of a given string. 
+
+function firstletter (str)
 {
-    // let alfa = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-    let alfa1 ='abcdefghijklmnopqrstuvwxyz';
     
-
-
-
 }
-stringPlus('Piotr');
+firstletter('');
