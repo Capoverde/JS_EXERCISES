9ef2731d535e6fc4f0846c8a805567eb6e7434c9
PiotrAs <piotr.slobinski@gmail.com>
2018-07-26 21:09:50 +0200

    solved   #79

--- root/scripts/01-basics.js
+++ root/scripts/01-basics.js
@@ -1626,3 +1626,23 @@
 //   return true;
 // }
 // console.log(notOneorThree([3,4,]));
+
+// ---------------- Exe #79 ---------------- //
+
+// Write a JavaScript program to test if a given array of integers contains 30 and 40 twice. The array length should be 0, 1, or 2
+
+// Their solution:
+
+// function twice3040(arra1) {
+//   let a = arra1[0],
+//       b = arra1[1];
+//   return (a === 30 && b === 30) || (a === 40 && b === 40);
+// }
+
+// console.log(twice3040([30, 30]));
+// console.log(twice3040([40, 40]));
+// console.log(twice3040([20, 20]));
+// console.log(twice3040([30]));
+
+
+
