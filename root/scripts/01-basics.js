5f8a8e33bb1a9017231aefd35a4bc2aad22c6621
PiotrAs <piotr.slobinski@gmail.com>
2018-07-26 23:18:01 +0200

    solved   #80

--- root/scripts/01-basics.js
+++ root/scripts/01-basics.js
@@ -1644,5 +1644,29 @@
 // console.log(twice3040([20, 20]));
 // console.log(twice3040([30]));
 
+// ---------------- Exe #80 ---------------- //
 
+// Write a JavaScript program to swap the first and last elements of a given array of integers. The array length should be at least 1.
+
+// My solution:
+
+// function change69 (arr)
+// {
+//   s1= arr.pop();
+//   s2=arr.shift(); 
+//   arr.unshift(s1);
+//   arr.push(s2)
+//   console.log(arr);
+// }
+// change69([6,2,3,4,5,9]);
+
+// Their solution:
+
+// function swap(arra) {
+//   [arra[0], arra[arra.length - 1]] = [arra[arra.length - 1], arra[0]];
+//   return arra;
+// }
+// console.log(swap([1, 2, 3, 4]));  
+// console.log(swap([0, 2, 1]));  
+// console.log(swap([3])); 
 
