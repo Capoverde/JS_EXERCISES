c4384cc347f503dfc13b870fc6a83caf3be5e05e
PiotrAs <piotr.slobinski@gmail.com>
2018-07-27 11:58:28 +0200

    solved   #81

--- root/scripts/01-basics.js
+++ root/scripts/01-basics.js
@@ -1670,3 +1670,14 @@
 // console.log(swap([0, 2, 1]));  
 // console.log(swap([3])); 
 
+// ---------------- Exe #81 ---------------- //
+
+// Write a JavaScript program to add two digits of a given positive integer of length two.
+
+
+function add_two_digits(n)
+  {
+     return n % 10 + Math.floor(n / 10);
+  };
+console.log(add_two_digits(25))
+console.log(add_two_digits(50)) 
