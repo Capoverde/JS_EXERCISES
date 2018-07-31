78adfae0f911f16358448cff50d9aa7e7c75b288
PiotrAs <piotr.slobinski@gmail.com>
2018-07-31 23:39:51 +0200

    solved #100

--- root/scripts/01-basics.js
+++ root/scripts/01-basics.js
@@ -1934,3 +1934,29 @@
 
 // console.log(Kth_greatest_in_array([1,2,3,4,5], 3))
 // console.log(Kth_greatest_in_array([-10,-25,-47,-36,0], 1))
+
+// ---------------- Exe #100 ---------------- //
+
+// Write a JavaScript program to find the maximum possible sum of some of its k consecutive numbers (numbers that follow each other in order.) of a given array of positive integers.
+
+// Their solution:
+
+// function array_max_consecutive_sum(nums, k) {
+//   let result = 0;
+//   let temp_sum = 0;
+//   for (var i = 0; i < k - 1; i++) {
+//     temp_sum += nums[i];
+//   }
+//   for (var i = k - 1; i < nums.length; i++) {
+//     temp_sum += nums[i];
+//     if (temp_sum > result) {
+//       result = temp_sum;
+//     }
+//     temp_sum -= nums[i - k + 1];
+//   }
+//   return result;
+// }
+
+// console.log(array_max_consecutive_sum([1, 2, 3, 14, 5], 2))
+// console.log(array_max_consecutive_sum([2, 3, 5, 1, 6], 3))
+// console.log(array_max_consecutive_sum([9, 3, 5, 1, 7], 2))
