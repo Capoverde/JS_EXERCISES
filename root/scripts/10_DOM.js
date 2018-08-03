4a3259d6cb40dd4285c5cd7ebf897f46a363be0e
PiotrAs <piotr.slobinski@gmail.com>
2018-08-03 14:34:00 +0200

    solved #1

---
+++ root/scripts/10_DOM.js
@@ -0,0 +1,12 @@
+// -------------- #1 -------------- //
+
+// Here is a sample html file with a submit button. Now modify the style of the paragraph text through javascript code.
+
+// const btn = document.getElementById('jsstyle');
+// let p = document.getElementById('text');
+
+// btn.addEventListener('click',
+// function change (){
+//  p.classList.toggle('change')
+// });
+
