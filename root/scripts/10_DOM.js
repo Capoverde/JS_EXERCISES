43d42cd3b9b944081b44990121191aaf8c970526
PiotrAs <piotr.slobinski@gmail.com>
2018-08-03 14:39:57 +0200

    solved #2

--- root/scripts/10_DOM.js
+++ root/scripts/10_DOM.js
@@ -10,3 +10,20 @@
 //  p.classList.toggle('change')
 // });
 
+// -------------- #2 -------------- //
+
+// Write a JavaScript function to get the values of First and Last name of the following form
+
+// The Solution:
+
+// function getFormvalue()
+// {
+//   var x=document.getElementById("form1");
+//   for (var i=0;i<x.length;i++)
+//   {
+//    if (x.elements[i].value!='Submit')
+//     {  
+//       console.log(x.elements[i].value);
+//      }  
+//    }
+// }
