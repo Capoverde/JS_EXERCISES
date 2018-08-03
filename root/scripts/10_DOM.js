03981a42ad12144c1e29c9c04a028d91ad7c45fb
PiotrAs <piotr.slobinski@gmail.com>
2018-08-04 00:28:29 +0200

    solved #5

--- root/scripts/10_DOM.js
+++ root/scripts/10_DOM.js
@@ -63,6 +63,18 @@
 
 // -------------- #5 -------------- //
 
+// Write a JavaScript function to add rows to a table.
+
+// The solution:
+
+// function insert_Row ()
+// {
+//   var x=document.getElementById('sampleTable').insertRow(0);
+//   var y = x.insertCell(0);
+//   var z = x.insertCell(1);
+//   y.innerHTML="New Cell1";
+//   z.innerHTML="New Cell2";
+// }
 
 
 
